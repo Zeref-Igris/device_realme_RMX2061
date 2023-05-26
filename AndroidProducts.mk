pri0818/falcon_device_realme_RMX2061

RMX2061: Merge RMX2061 device tree into common tree

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2020 The DotOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_RMX2061.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_RMX2061-user \
+    derp_RMX2061-userdebug \
+    derp_RMX2061-eng 

Zeref-Igris/device_realme_RMX2061

Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
Signed-off-by: priiii0818 <priyanshusinghal0818@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2020 The DotOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_RMX2061.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_RMX2061-user \
+    lineage_RMX2061-userdebug \
+    lineage_RMX2061-eng 