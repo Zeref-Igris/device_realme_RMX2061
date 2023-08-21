2723b33a3127c7699bf46c3a24a5c36740b7810e
Zeref_Igris <igriszeref.002@gmail.com>
2023-08-21 09:23:25 +0000

    Superior OS Bringup
    Signed-off-by: Zeref_Igris <igriszeref.002@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2061.mk
+    $(LOCAL_DIR)/superior_RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX2061-user \
-    lineage_RMX2061-userdebug \
-    lineage_RMX2061-eng 
+    superior_RMX2061-user \
+    superior_RMX2061-userdebug \
+    superior_RMX2061-eng 
