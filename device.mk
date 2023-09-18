4e67e063b052482ed8a869a189dc38aabcb246af
mikeNG <mikeioannina@gmail.com>
2023-09-18 16:36:05 +0530

    RMX2061: Build Lineage Health HAL
    Signed-off-by: BalayBera <balaybera@proton.me>
    Signed-off-by: Zeref_Igris <igriszeref.002@gmail.com>
    Signed-off-by: Zeref-Igris <igriszeref.002@gmail.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sm7125
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
